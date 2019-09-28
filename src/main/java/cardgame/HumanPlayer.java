9914ee6536f6fa11cc4570204eca60f8450f5b7b
Joyce Chu <jujoyce12913804@gmail.com>
2019-09-28 15:08:30 -0500

    Add HumanPlayer, NonPlayer, Tournament classes

---
+++ src/main/java/cardgame/HumanPlayer.java
@@ -0,0 +1,6 @@
+package cardgame;
+
+public class HumanPlayer extends PlayerStrategy{
+
+
+}
---
+++ src/main/java/cardgame/Tournament.java
@@ -0,0 +1,11 @@
+package cardgame;
+
+public class Tournament {
+
+    private static final int ENDING_POINTS = 200;
+
+    public static void main(String[] args) {
+
+
+    }
+}
