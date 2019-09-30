65af5027bd397082ad79f6fb148f0983b2ee2048
Joyce Chu <jujoyce12913804@gmail.com>
2019-09-30 14:55:31 -0500

    Put all the set up in initialization method in Tournament1 class.

--- src/main/java/cardgame/Tournament1.java
+++ src/main/java/cardgame/Tournament1.java
@@ -7,56 +7,27 @@ public class Tournament1 {
 
 
     private static final int ENDING_POINTS = 200;
-    private static final int TOTAL_PLAYER = 4;
+    private static final int TOTAL_PLAYER = 2;
     private static final int INITIAL_CARDS = 5;
     private static final int TOP_CARD = 0;
 
+    private boolean cheat;
+    private int cheater;
+    private int currentHighestPoint;
+    private int totalGame;
+    private Card.Suit changedSuit;
+    private List<PlayerStrategy> playerList;
+    private List<Integer> scoreBoard;
+    private List<Integer> winCount;
+    private List<Card> drawPile;
+    private List<Card> discardPile;
+
     public static void main(String[] args) {
 
-        boolean cheat = false;
-        int cheater;
-        int currentHighestPoint = 0;
-        int totalGame = 0;
-        Card.Suit changedSuit = null;
 
         System.out.println("Welcome to the Crazy 8's card game.\n");
 
-        List<PlayerStrategy> playerList = new ArrayList<>();
-        playerList.add(new NonPlayer1());
-        playerList.add(new NonPlayer1());
-        playerList.add(new NonPlayer2());
-        playerList.add(new NonPlayer2());
-
         //initialize
-        for(int i = 0; i < playerList.size(); i++) {
-            playerList.get(i).init(i, opponentIds(i));
-        }
-
-        //Create score board
-        List<Integer> scoreBoard = new ArrayList<>();
-        List<Integer> winCount = new ArrayList<>();
-        for(int i = 0; i < TOTAL_PLAYER; i ++) {
-            scoreBoard.add(0);
-            winCount.add(0);
-        }
-
-
-        //deal 5 cards to each player
-        List<Card> drawPile = new ArrayList<>(Card.getDeck());
-        Collections.shuffle(drawPile);
-
-        for(PlayerStrategy player : playerList) {
-            player.receiveInitialCards(drawPile.subList(TOP_CARD, INITIAL_CARDS));
-            drawPile.subList(TOP_CARD, INITIAL_CARDS).clear();
-        }
-
-        //get first card of discard pile
-        List<Card> discardPile = new ArrayList<>();
-        while(drawPile.get(TOP_CARD).getRank() == Card.Rank.EIGHT) {
-            Collections.shuffle(drawPile);
-        }
-        discardPile.add(drawPile.get(TOP_CARD));
-        drawPile.remove(TOP_CARD);
 
         //Play game
         do {
@@ -87,6 +58,48 @@ public class Tournament1 {
         }else{
             System.out.println(cheater + " cheated, this is the end of this is the end of this tournament, thank you for playing.");
         }
+    }
+
+    private void initialized() {
+
+        cheat = false;
+        currentHighestPoint = 0;
+        totalGame = 0;
+        changedSuit = null;
+
+
+        playerList = new ArrayList<>();
+        playerList.add(new NonPlayer1());
+        playerList.add(new NonPlayer2());
+
+        for(int i = 0; i < playerList.size(); i++) {
+            playerList.get(i).init(i, opponentIds(i));
+        }
+
+        //Create score board
+        scoreBoard = new ArrayList<>();
+        winCount = new ArrayList<>();
+        for(int i = 0; i < TOTAL_PLAYER; i ++) {
+            scoreBoard.add(0);
+            winCount.add(0);
+        }
+
+        //deal 5 cards to each player
+        drawPile = new ArrayList<>(Card.getDeck());
+        Collections.shuffle(drawPile);
+
+        for(PlayerStrategy player : playerList) {
+            player.receiveInitialCards(drawPile.subList(TOP_CARD, INITIAL_CARDS));
+            drawPile.subList(TOP_CARD, INITIAL_CARDS).clear();
+        }
+
+        //get first card of discard pile
+        discardPile = new ArrayList<>();
+        while(drawPile.get(TOP_CARD).getRank() == Card.Rank.EIGHT) {
+            Collections.shuffle(drawPile);
+        }
+        discardPile.add(drawPile.get(TOP_CARD));
+        drawPile.remove(TOP_CARD);
     }
 
     private static List<Integer> opponentIds(int playerNumber) {
